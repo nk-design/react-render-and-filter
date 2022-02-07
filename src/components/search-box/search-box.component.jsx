30763f4f61f58c944f1ef70388d1b9e06e59d344
Mykola Kovtun <mykola.kovtun@evolv.ai>
2022-02-07 18:44:04 +0200

    Updates

--- src/components/search-box/search-box.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./search-box.styles.css"
-
-export const SearchBox = ({placeholder, searchHandle}) =>(
-    <input className="search" type="search" placeholder = {placeholder} onChange = {searchHandle}/>
-)
